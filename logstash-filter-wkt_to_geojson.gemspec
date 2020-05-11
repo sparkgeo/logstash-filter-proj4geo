8d820644e24a401f6f28b16dfe5012cfd8b49f76
Alain St. Pierre <alain@sparkgeo.com>
2020-05-11 13:38:07 -0700

    initial commit from https://github.com/kalmas/logstash-filter-wkt_to_geojson

---
+++ logstash-filter-wkt_to_geojson.gemspec
@@ -0,0 +1,24 @@
+Gem::Specification.new do |s|
+  s.name          = 'logstash-filter-wkt_to_geojson'
+  s.version       = '0.1.4'
+  s.licenses      = ['Apache-2.0']
+  s.summary       = 'Logstash filter to convert WKT geography shapes to GeoJSON.'
+  s.homepage      = 'https://github.com/kalmas/logstash-filter-wkt_to_geojson'
+  s.authors       = ['kalmas']
+  s.email         = 'kylealmas@gmail.com'
+  s.require_paths = ['lib']
+
+  # Files
+  s.files = Dir['lib/**/*','spec/**/*','vendor/**/*','*.gemspec','*.md','CONTRIBUTORS','Gemfile','LICENSE','NOTICE.TXT']
+   # Tests
+  s.test_files = s.files.grep(%r{^(test|spec|features)/})
+
+  # Special flag to let us know this is actually a logstash plugin
+  s.metadata = { "logstash_plugin" => "true", "logstash_group" => "filter" }
+
+  s.add_dependency 'georuby', '2.5.2'
+
+  s.add_runtime_dependency "logstash-core-plugin-api", ">= 1.60", "<= 2.99"
+  
+  s.add_development_dependency 'logstash-devutils', '1.3.3'
+end
---
+++ spec/filters/wkt_to_geojson_spec.rb
@@ -0,0 +1,105 @@
+# encoding: utf-8
+require_relative '../spec_helper'
+require "logstash/filters/wkt_to_geojson"
+
+describe LogStash::Filters::WktToGeojson do
+  describe "Convert WKT to GeoJSON" do
+    let(:config) do <<-CONFIG
+      filter {
+        wkt_to_geojson {
+          source => "geometry"
+        }
+      }
+    CONFIG
+    end
+
+    sample("geometry" => "POLYGON ((0.0 0.0, 5.0 0.0, 5.0 5.0, 0.0 5.0, 0.0 0.0), (2.0 2.0, 2.0 3.0, 3.0 3.0, 3.0 2.0, 2.0 2.0))") do
+      expect(subject.get('geo_json')).to eq("{\"type\":\"Polygon\",\"coordinates\":[[[0.0,0.0],[5.0,0.0],[5.0,5.0],[0.0,5.0],[0.0,0.0]],[[2.0,2.0],[2.0,3.0],[3.0,3.0],[3.0,2.0],[2.0,2.0]]]}")
+      expect(subject).to_not include("bbox")
+    end
+  end
+
+  describe "Calculate Bounding Box" do
+    let(:config) do <<-CONFIG
+      filter {
+        wkt_to_geojson {
+          source => "geometry"
+          bbox => "bbox"
+        }
+      }
+    CONFIG
+    end
+
+    sample("geometry" => "LINESTRING(10 40, 40 30, 20 20, 30 10)") do
+      expect(subject.get('bbox')).to eq("[10.0,10.0,40.0,40.0]")
+    end
+  end
+
+  describe "Write to target field" do
+    let(:config) do <<-CONFIG
+      filter {
+        wkt_to_geojson {
+          source => "geometry"
+          target => "geoff_jason"
+        }
+      }
+    CONFIG
+    end
+
+    sample("geometry" => "GEOMETRYCOLLECTION(POINT(3 5), LINESTRING(-2 0, -3 -4))") do
+      expect(subject.get('geoff_jason')).to eq("{\"type\":\"GeometryCollection\",\"geometries\":[{\"type\":\"Point\",\"coordinates\":[3.0,5.0]},{\"type\":\"LineString\",\"coordinates\":[[-2.0,0.0],[-3.0,-4.0]]}]}")
+      expect(subject.get('geometry')).to eq("GEOMETRYCOLLECTION(POINT(3 5), LINESTRING(-2 0, -3 -4))")
+    end
+  end
+
+  describe "Handle invalid input" do
+    let(:config) do <<-CONFIG
+      filter {
+        wkt_to_geojson {
+          source => "geometry"
+          bbox => "bbox"
+        }
+      }
+    CONFIG
+    end
+
+    sample("geometry" => "MULTIPOLYGON((()))") do
+      expect(subject).to_not include("geo_json")
+      expect(subject.get('geometry')).to eq("MULTIPOLYGON((()))")
+      expect(subject.get('tags')).to include('_wkt_parse_failure')
+    end
+  end
+
+  describe "Handle nil input" do
+    let(:config) do <<-CONFIG
+      filter {
+        wkt_to_geojson {
+          source => "geometry"
+          tag_on_failure => [ "oppsie" ]
+        }
+      }
+    CONFIG
+    end
+
+    sample("geometry" => nil) do
+      expect(subject).to_not include("geo_json")
+      expect(subject.get('geometry')).to eq(nil)
+      expect(subject.get('tags')).to include('oppsie')
+    end
+  end
+
+  describe "Handle absent field" do
+    let(:config) do <<-CONFIG
+      filter {
+        wkt_to_geojson {
+          source => "not_a_field"
+        }
+      }
+    CONFIG
+    end
+
+    sample("geometry" => "GEOMETRYCOLLECTION(POINT(3 5), LINESTRING(-2 0, -3 -4))") do
+      expect(subject).to_not include("geo_json")
+    end
+  end
+end
